d574c9fb35c6f988b41ad0565857ff25adc61dd9
Jiseok <chae0_@naver.com>
2023-09-29 02:05:44 +0900

    Empty

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,7 +2,32 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
-${reset}
+  ${reset}
+
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  width: 100%;
+  height: 100%;
+  font-family: 'Pretendard', sans-serif;  /* or any font you prefer */
+}
+
+#app {  /* Assuming your root div has an id of "app" */
+  width: 375px;
+  height: 812px;
+  margin: 0 auto;   /* Center the chatroom horizontally */
+  background-color: white;
+  overflow: hidden;  /* Ensures nothing overflows the specified dimensions */
+}
+
+
+
+
+
 `;
 
 export default GlobalStyle;
